35d684c5513b92940a33358822a9d85f45ef782e
MennaAnany <mennaanany@icloud.com>
2023-11-04 20:20:10 +0200

    Added Helpers

---
+++ API/Helpers/ProductParams.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Helpers
+{
+    public class ProductParams : PaginationParams
+    {
+        public string Category { get; set; }
+        public string SearchFilter { get; set; }
+        public string PriceRange { get; set; }
+    }
+}
